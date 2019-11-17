43f69dbdfb78a5449eb40f90708ce563acf8a4d1
馬健凱 <koovakevy@gmail.com>
2019-11-18 00:49:58 +0800

    Bug-fixing and finish BSTree::iterator

--- hw5/src/util/bst.h
+++ hw5/src/util/bst.h
@@ -41,9 +41,10 @@ template <class T>
 class BSTree
 {
    // TODO: design your own class!!
+public:
    BSTree() {
       _root = new BSTreeNode<T>(T());
-      _root->_left = _root->_right = _root;
+      _root->_parent = _root; // _root is a dummy node
    }
    ~BSTree() { clear(); delete _root; }
 
@@ -72,14 +73,14 @@ class BSTree
       BSTreeNode<T>* _node;
    };
 
-iterator begin() const {}
-iterator end() const {}
+iterator begin() const { return iterator(findMin(_root)); }
+iterator end() const { return iterator(_root->_parent); }
 bool empty() const {
-   return (_root == 0);
+   return (_root->_parent == _root);
 }
 
 size_t size() const {
-
+   return _size;
 }
 
 void insert(const T& x) {
@@ -100,11 +101,27 @@ bool erase(iterator pos) {
 
 bool erase(const T& x) {
 
+BSTreeNode<T>* findMin(BSTreeNode<T>* start) const {
+   if (empty()) { return 0; }
+   BSTreeNode<T>* goingLeft = start;
+   while (goingLeft->_left != _root->_parent) {
+      goingLeft = goingLeft->_left;
+   }
+   return goingLeft;
 }
 
-iterator find(const T& x) {
+BSTreeNode<T>* findMax(BSTreeNode<T>* start) const {
+   if (empty()) { return 0; }
+   BSTreeNode<T>* goingRight = start;
+   while (goingRight->_right != _root->_parent) {
+      goingRight = goingRight->_right;
+   }
+   return goingRight;
+}
 
 }
+
+void sort() const {}
 
 void print() const {
    string myBlank = "";
@@ -112,12 +129,12 @@ void print() const {
 }
 
 void print(BSTreeNode<T>* myRoot, string myBlank) const {
-   cerr << myBlank << myRoot->_data << endl;
+   cout << myBlank << myRoot->_data << endl;
    myBlank = myBlank + "  ";
-   if (myRoot->_left != _root) { print(myRoot->_left, myBlank); }
-   else { cerr << myBlank << "[0]" << endl; }
-   if (myRoot->_right != _root) { print(myRoot->_right, myBlank); }
-   else { cerr << myBlank << "[0]" << endl; }
+   if (myRoot->_left != _root->_parent) { print(myRoot->_left, myBlank); }
+   else { cout << myBlank << "[0]" << endl; }
+   if (myRoot->_right != _root->_parent) { print(myRoot->_right, myBlank); }
+   else { cout << myBlank << "[0]" << endl; }
 }
 
 private:
